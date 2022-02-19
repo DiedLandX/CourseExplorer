ed62bea62a56de1e391e1d1310bebf178e924349
DiedlandX <leventeberes4@gmail.com>
2022-02-19 16:50:07 +0100

    Prepare Heroku

--- backend/app.js
+++ backend/app.js
@@ -66,5 +66,13 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
 });
+const path = require("path");
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
+});
 module.exports = app;
